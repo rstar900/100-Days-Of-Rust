6ecd0caee7ed564c1f646eaf364dc845d45cca2d
rstar900 <rachitgarg900@gmail.com>
2024-08-15 01:09:26 +0200

    Added day 08

--- Week-02/Day-08_Letter-Combinations-Of-A-Phone-Number/letter_combinations/src/main.rs
+++ Week-02/Day-08_Letter-Combinations-Of-A-Phone-Number/letter_combinations/src/main.rs
@@ -1,5 +1,28 @@
-use std::io;
+use std::io::{self, Write};
 use std::collections::HashMap;
+
+// Helper function with recursion
+fn letter_combinations_helper(input: &str, state: &mut String, output: &mut Vec<String>, key_map: &HashMap<char, Vec<char>>) {
+
+    // Base case
+    if input.is_empty() {
+        // Append the state so far to the output vector
+        output.push((*state).clone());
+        return;
+
+    } else {
+        // Iterate the values of the key which is the first character in input substring
+        let letters = key_map.get(&input.chars().nth(0).expect("No character at 0th position")).expect("Undefined key");
+        for letter in letters {
+            // Append the character to the existing state
+            state.push(*letter);
+            // start the process for the next character of input substring recursively
+            letter_combinations_helper(&input[1..], state, output, key_map);
+            // remove the character from the state as state is a common reference
+            state.pop();
+        }
+    }   
+}
 
 
 fn letter_combinations(input: &str) -> Vec<String> {
@@ -15,16 +38,18 @@ fn letter_combinations(input: &str) -> Vec<String> {
     key_map.insert('8', vec!['t', 'u', 'v']);
     key_map.insert('9', vec!['w', 'x', 'y', 'z']);
 
-    println!("{key_map:?}");
-
-    // TODO: complete the function definition
+    letter_combinations_helper(input, &mut String::new(), &mut output, &key_map);
 
     output
 }
 
 fn main() {
-    // Just testing what the created map looks like
-    letter_combinations("123");
+    let mut input = String::new();
+    print!("Enter the input digits: ");
+    io::stdout().flush().expect("Error flushing the STDOUT!");
+    io::stdin().read_line(&mut input).expect("Error reading input from STDIN!");
+    let output = letter_combinations(&input.trim());
+    println!("The possible letter combinations are: {:?}", output);
 }
 
 // Tests are below
@@ -32,5 +57,7 @@ fn main() {
 
 #[test]
 fn test_letter_combinations() {
-    // TODO testing logic
+    assert_eq!(letter_combinations("23"), vec!["ad","ae","af","bd","be","bf","cd","ce","cf"]);
+    assert_eq!(letter_combinations(""), vec![""]);
+    assert_eq!(letter_combinations("2"), vec!["a","b","c"]);
 }
